aaa5e8b156497b91b01b6621e3ff2899d5f1dc83
Jonas Guinami <magdrawbr@gmail.com>
2025-12-17 01:43:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://latina-bare-chronic-franklin.trycloudflare.com";
+const API_BASE_URL = "https://saints-sanyo-stretch-outputs.trycloudflare.com";
 
 let userSession = { isLoggedIn: false, username: 'visitante' };
 
@@ -106,10 +106,7 @@ function renderComments() {
             <form class="comment-form" onsubmit="sendComment(event, '${pid}')">
                 <input class="comment-input" type="text" placeholder="Escrever log..." required>
                 <button type="submit" class="btn-send">
-                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="5" y1="12" x2="19" y2="12"></line>
-                        <polyline points="12 5 19 12 12 19"></polyline>
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1792" height="1792" viewBox="0 0 1792 1792"><path fill="currentColor" d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45q-14 8-31 8q-11 0-24-5l-453-185l-242 295q-18 23-49 23q-13 0-22-4q-19-7-30.5-23.5T640 1728v-349l864-1059l-1069 925l-395-162q-37-14-40-55q-2-40 32-59L1696 9q15-9 32-9q20 0 36 11"/></svg>
                 </button>
             </form>
         `;
